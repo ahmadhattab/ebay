75cc8fc649a104bcac2a62c1fbf29496230ae3de
root <root@wrk018.corp.markavip.net>
2013-12-21 22:48:20 +0300

    git ignore modif

--- app/code/local/Jordanshopper/Seller/Block/Seller.php
+++ app/code/local/Jordanshopper/Seller/Block/Seller.php
@@ -7,7 +7,8 @@ class Jordanshopper_Seller_Block_Seller extends Mage_Core_Block_Template
 	}
 	public function getCategoryTree()
     	{        
-	        $object = new Mage_Catalog_Block_Navigation();
+	        // make it as parent an child
+                $object = new Mage_Catalog_Block_Navigation();
 	        $actualCategoryId = $object->getCurrentCategory()->getId();        
 	        $actualCategory = Mage::getModel('catalog/category')->load($actualCategoryId);
 	        $subCategories = explode(',', $actualCategory->getChildren());        
