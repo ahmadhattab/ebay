fe93dc5795d9ec2bf952a8611fce4f133acb50c5
salameh <salameh@salameh-yasin.(none)>
2014-01-16 10:08:18 +0200

    add listing tab

---
+++ app/code/local/Jordanshopper/Seller/Block/Adminhtml/Customer/Edit/Listing.php
@@ -0,0 +1,10 @@
+<?php
+
+class Jordanshopper_Seller_Block_Adminhtml_Customer_Edit_Listing 
+    extends Mage_Adminhtml_Block_Template
+{
+    public function initForm()
+    {
+        echo 'ddddd';
+    }
+}
