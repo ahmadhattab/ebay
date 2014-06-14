837a8218c736032661cd8287eb513d1e6712029d
Ahmad Hattab <ahmhattab@gmail.com>
2014-06-14 15:55:11 +0300

    Modify Code

--- app/code/local/Jordanshopper/Seller/controllers/ItemController.php
+++ app/code/local/Jordanshopper/Seller/controllers/ItemController.php
@@ -207,7 +207,7 @@ class Jordanshopper_Seller_ItemController extends Mage_Core_Controller_Front_Act
 	{
 		$session    = Mage::getSingleton('core/session');
 		$code = $this->getRequest()->getParam('discountCode');
-		if ($code == 'sell_now')
+		if ($code == 'ramadan sale')
 		{
 			$itemId = $this->getRequest()->getParam('seller_id');
 			$item = Mage::getModel('seller/seller')->load($itemId);
