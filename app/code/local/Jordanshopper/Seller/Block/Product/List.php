6f2fa030b662a83604299ae7088311db3b248d2c
Ahmad Hattab <ahmhattab@gmail.com>
2014-08-20 21:22:54 +0300

    Fix bugs

--- app/code/local/Jordanshopper/Seller/Block/Product/List.php
+++ app/code/local/Jordanshopper/Seller/Block/Product/List.php
@@ -1,60 +1,62 @@
 <?php
 class Jordanshopper_Seller_Block_Product_List extends Mage_Catalog_Block_Product_List
 {
- protected function _getProductCollection()
-    {
-        if (is_null($this->_productCollection)) {
-            $layer = $this->getLayer();
-            /* @var $layer Mage_Catalog_Model_Layer */
-            if ($this->getShowRootCategory()) {
-                $this->setCategoryId(Mage::app()->getStore()->getRootCategoryId());
-            }
+	protected function _getProductCollection()
+	{
+		if (is_null($this->_productCollection)) {
+			$layer = $this->getLayer();
+			/* @var $layer Mage_Catalog_Model_Layer */
+			if ($this->getShowRootCategory()) {
+				$this->setCategoryId(Mage::app()->getStore()->getRootCategoryId());
+			}
 
-            // if this is a product view page
-            if (Mage::registry('product')) {
-                // get collection of categories this product is associated with
-                $categories = Mage::registry('product')->getCategoryCollection()
-                    ->setPage(1, 1)
-                    ->load();
-                // if the product is associated with any category
-                if ($categories->count()) {
-                    // show products from this category
-                    $this->setCategoryId(current($categories->getIterator()));
-                }
-            }
+			// if this is a product view page
+			if (Mage::registry('product')) {
+				// get collection of categories this product is associated with
+				$categories = Mage::registry('product')->getCategoryCollection()
+				->setPage(1, 1)
+				->load();
+				// if the product is associated with any category
+				if ($categories->count()) {
+					// show products from this category
+					$this->setCategoryId(current($categories->getIterator()));
+				}
+			}
 
-            $origCategory = null;
-            if ($this->getCategoryId()) {
-                $category = Mage::getModel('catalog/category')->load($this->getCategoryId());
-                if ($category->getId()) {
-                    $origCategory = $layer->getCurrentCategory();
-                    $layer->setCurrentCategory($category);
-                }
-            }
-            
-            if ($this->getRequest()->getParam('id'))
-            {
-            	$sellerId = $this->getRequest()->getParam('id');
-            }
-            else
-            {
-            	$sellerId = Mage::getSingleton('customer/session')->getCustomerId();
-            }
-            
-            $collection = Mage::getModel('catalog/product')->getCollection()
-            				->addAttributeToSelect('*')
-            				->addAttributeToFilter('status', array('eq'=> 1))
-					->addAttributeToFilter('seller_id',array('eq' => $sellerId));
-            //$this->_productCollection = $layer->getProductCollection();
+			$origCategory = null;
+			if ($this->getCategoryId()) {
+				$category = Mage::getModel('catalog/category')->load($this->getCategoryId());
+				if ($category->getId()) {
+					$origCategory = $layer->getCurrentCategory();
+					$layer->setCurrentCategory($category);
+				}
+			}
+
+			if ($this->getRequest()->getParam('id'))
+			{
+				$sellerId = $this->getRequest()->getParam('id');
+			}
+			else
+			{
+				$sellerId = Mage::getSingleton('customer/session')->getCustomerId();
+			}
+
+			$collection = Mage::getModel('catalog/product')->getCollection()
+			->addAttributeToSelect('*')
+			->addAttributeToFilter('status', array('eq'=> 1))
+			->addAttributeToFilter('seller_id',array('eq' => $sellerId));
+			Mage::getSingleton('cataloginventory/stock')->addInStockFilterToCollection($collection);
+				
+			//$this->_productCollection = $layer->getProductCollection();
 			$this->_productCollection = $collection;
-            $this->prepareSortableFieldsByCategory($layer->getCurrentCategory());
+			$this->prepareSortableFieldsByCategory($layer->getCurrentCategory());
 
-            if ($origCategory) {
-                $layer->setCurrentCategory($origCategory);
-            }
-        }
+			if ($origCategory) {
+				$layer->setCurrentCategory($origCategory);
+			}
+		}
 
-        return $this->_productCollection;
-    }
-	
+		return $this->_productCollection;
+	}
+
 }
